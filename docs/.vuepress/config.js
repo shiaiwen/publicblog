847a16ec059c6f3962ff0689abcc3f8ef2157706
shiaiwen <2474577454@qq.com>
2021-01-02 21:26:56 +0800

    q

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,57 @@
+module.exports = {
+    title: '努力的沧州人',
+    description: '努力的沧州人的技术博客',
+    head: [
+        [
+            'meta',
+            { name: 'keywords', content: '前端,vuepress博客,努力的沧州人' },
+        ],
+        ['meta', { name: '努力的沧州人' }],
+        ['link', { rel: 'icon', href: './favicon.ico' }],
+    ],
+    themeConfig: {
+        logo: '/assets/img/hero.png',
+        // navbar: false,
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '关于', link: '/about/' },
+            { text: '链接', link: 'https://www.vuepress.cn/' },
+            // {
+            //   text: 'Languages',
+            //   items: [
+            //     { text: 'Group1', items: [  { text: '主页', link: '/' },
+            //     { text: '关于', link: '/about/' }] },
+            //     { text: 'Group2', items: [  { text: '主页', link: '/' },
+            //     { text: '关于', link: '/about/' }] }
+            //   ]
+            // }
+        ],
+
+        // 路径
+
+        // sidebar: [
+        //   '/',
+        //   '/page-a',
+        //   ['/page-b', 'Explicit link text']
+        // ]
+
+        // sidebar: [
+        //   '/',
+        //   '/about/',
+        //   {
+        //     title: 'css集合',   // 必要的
+        //     path: '/css/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        //     collapsable: false, // 可选的, 默认值是 true,
+        //     sidebarDepth: 1,    // 可选的, 默认值是 1
+        //     children: [
+        //       '/css/css1',
+        //       '/css/css2',
+        //       '/css/css3',
+        //     ]
+        //   }]
+
+        themeConfig: {
+          lastUpdated: '更新时间', // string | boolean
+        }
+    },
+}
---
+++ docs/README.md
@@ -0,0 +1,17 @@
+---
+home: true
+heroImage: /assets/img/hero.png
+heroText: 凝聚沉淀
+tagline: 业精于勤荒于嬉，行成于思毁于随
+actionText: Start →
+actionLink: /about/
+features:
+- title: 专心致志，夯实基础
+  details: 天道酬勤，要有匠心精神，要相信积累的力量，伟大的事业是撸起袖子干出来的
+- title: 奋思进取，实事求是
+  details: 要避免故步自封，事物具有多面性，要站在客观角度去思考问题
+- title: 厚德载物，自强不息
+  details: 加强身体锻炼，加强思想教育，多做事多参与多赞扬，少评论少旁观少批评
+footer: stay hungry | Copyright © 2019-present Evan You
+
+---
