870089b369e3a969df6eacb87372e31f7d99baa0
shiaiwen <2474577454@qq.com>
2021-01-05 21:55:04 +0800

    feat:个人简介

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -46,20 +46,22 @@ module.exports = {
         //   ['/page-b', 'Explicit link text']
         // ]
 
-        // sidebar: [
-        //   '/',
-        //   '/about/',
-        //   {
-        //     title: 'css集合',   // 必要的
-        //     path: '/css/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-        //     collapsable: false, // 可选的, 默认值是 true,
-        //     sidebarDepth: 1,    // 可选的, 默认值是 1
-        //     children: [
-        //       '/css/css1',
-        //       '/css/css2',
-        //       '/css/css3',
-        //     ]
-        //   }]
+        sidebar: [
+          '/',
+          '/about/',
+          // {
+          //   title: 'css集合',   // 必要的
+          //   path: '/css/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          //   collapsable: false, // 可选的, 默认值是 true,
+          //   sidebarDepth: 1,    // 可选的, 默认值是 1
+          //   children: [
+          //     '/css/css1',
+          //     '/css/css2',
+          //     '/css/css3',
+          //   ]
+          // }
+        
+        ]
 
 
     },
