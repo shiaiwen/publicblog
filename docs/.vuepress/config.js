c3588ce6c15a5967b1e98d716d4e70931cbe2df7
shiaiwen <2474577454@qq.com>
2021-01-08 10:43:32 +0800

    添加vuepress 教程

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -49,17 +49,18 @@ module.exports = {
         sidebar: [
           '/',
           '/about/',
-          // {
-          //   title: 'css集合',   // 必要的
-          //   path: '/css/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-          //   collapsable: false, // 可选的, 默认值是 true,
-          //   sidebarDepth: 1,    // 可选的, 默认值是 1
-          //   children: [
-          //     '/css/css1',
-          //     '/css/css2',
-          //     '/css/css3',
-          //   ]
-          // }
+
+          {
+            title: '工具',   // 必要的
+            path: '/tools/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+            collapsable: false, // 可选的, 默认值是 true,
+            sidebarDepth: 1,    // 可选的, 默认值是 1
+            children: [
+              '/tools/vuepress',
+              // '/css/css2',
+              // '/css/css3',
+            ]
+          }
         
         ]
 
